214543b9789edef7f6475bc8f341fabdd23ae46a
vs0din <39313554+vs0din@users.noreply.github.com>
2019-01-23 14:17:49 +0200

    'nopostscreen'

--- src/components/PostsList/PostsList.js
+++ src/components/PostsList/PostsList.js
@@ -5,50 +5,54 @@ import Post from '../Post/Post'
 import {changeCurrPostId, changeCurrPostTitle} from "../../actions";
 
 class PostsList extends Component {
-	componentDidMount() {
-		this.props.changeCurrPostId(this.props.posts[0].id)
-		this.props.changeCurrPostTitle(this.props.posts[0].title)
-	}
+  componentDidMount() {
+    if(this.props.posts.length > 0){
+      this.props.changeCurrPostId(this.props.posts[0].id)
+      this.props.changeCurrPostTitle(this.props.posts[0].title)
+    }
+  }
 
-	render() {
-		return (
-			<div className="list row"
-           ref={(el) => { this.listEl = el; }}>
-				{
-					this.props.posts.map(post =>
-						<Post
-							key={post.id}
-							id={post.id}
-							title={post.title}
-							commentsCount={post.commentsCount}
-						/>
-					)
-				}
-			</div>
-		);
-	}
+  render() {
+    return (
+        <div className="list row"
+             ref={(el) => {
+               this.listEl = el;
+             }}>
+          {
+            this.props.posts.map(post =>
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    title={post.title}
+                    commentsCount={post.commentsCount}
+                />
+            )
+          }
+        </div>
+    );
+  }
 };
 
 function mapStateToProps(state) {
-	return {
-		posts: state.posts,
-		visibleScreen: state.visibleScreen
-	}
+  return {
+    posts: state.posts,
+    visibleScreen: state.visibleScreen
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		changeCurrPostId: (currPostId) => {
-			dispatch(changeCurrPostId({
-				currPostId: currPostId
-			}));
-		},
-		changeCurrPostTitle: (currPostTitle) => {
-			dispatch(changeCurrPostTitle({
-				currPostTitle: currPostTitle
-			}));
-		}
-	}
+  return {
+    changeCurrPostId: (currPostId) => {
+      dispatch(changeCurrPostId({
+        currPostId: currPostId
+      }));
+    },
+    changeCurrPostTitle: (currPostTitle) => {
+      dispatch(changeCurrPostTitle({
+        currPostTitle: currPostTitle
+      }));
+    }
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsList);
